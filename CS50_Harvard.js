ab10f9be16a785d10e91d41229459c64e086d944
Douglas <douglas0fac@gmail.com>
2022-04-15 12:06:23 -0300

    Create CS50_Harvard.js

---
+++ CS50_Harvard.js
@@ -0,0 +1,8 @@
+function ola(){
+    if (document.querySelector('h1').innerHTML === "Olá Marilene!"){
+        document.querySelector('h1').innerHTML = "Sayonara!";
+    }
+    else{
+        document.querySelector('h1').innerHTML = "Olá Marilene!";
+    }
+}
